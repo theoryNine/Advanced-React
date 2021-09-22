ce51aef6e6f444b334e02d4b11d04c3789652daa
theoryNine <anselcolvin@gmail.com>
2021-09-21 22:29:58 -0700

    Updates

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,13 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import NProgress from 'nprogress';
+import PropTypes from 'prop-types';
+import Router from 'next/router';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -7,3 +16,8 @@ export default function MyApp({ Component, pageProps }) {
     </Page>
   );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any,
+};
